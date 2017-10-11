40addf24cf20359000be63ead12cbec0dce36997
Adi Zhavo <adi@gram.gs>
2017-10-11 12:59:28 +0100

    Add log wrapper to filter between adhoc/release builds

---
+++ Assets/Game/Scripts/Framework/Logger.cs
@@ -0,0 +1,41 @@
+﻿namespace Framework.Log
+{
+    public static class Logger 
+    {
+        public static void DebugLog(string message)
+        {
+            #if ADHOC || UNITY_EDITOR
+            UnityEngine.Debug.Log(message);
+            #endif
+        }
+
+        public static void DebugError(string message)
+        {
+            #if ADHOC || UNITY_EDITOR
+            UnityEngine.Debug.LogError(message);
+            #endif
+        }
+
+        public static void DebugWarning(string message)
+        {
+            #if ADHOC || UNITY_EDITOR
+            UnityEngine.Debug.LogWarning();
+            #endif
+        }
+
+        public static void Log(string message)
+        {
+            UnityEngine.Debug.Log(message);
+        }
+
+        public static void Error(string message)
+        {
+            UnityEngine.Debug.LogError(message);
+        }
+
+        public static void Warning(string message)
+        {
+            UnityEngine.Debug.LogWarning();
+        }
+    }
+}
