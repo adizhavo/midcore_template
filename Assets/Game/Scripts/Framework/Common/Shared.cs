7e164bad5208a297647eabaa7603b894514d182b
Adi Zhavo <adi@gram.gs>
2017-10-11 18:49:50 +0100

    Move shared framework classes under Common folder

---
+++ Assets/Game/Scripts/Framework/Common/Shared.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+public partial class Constants
+{
+    public const string APP_CONFIG_ID = "app_config";
+}
+
+namespace Framework
+{
+    // DataBooter will load this and add it to the database
+    public class ApplicationConfig
+    {
+        public string version;
+        public string assetManifestPath;
+    }
+
+    // Exported from the sheets
+    // DataBooter will loop through the assets and add them to the data base as read-only values
+    public class AssetManifest
+    {
+        public List<Asset> root;
+    }
+
+    // DataBooter will load the assets and add it to the database
+    public class Asset
+    {
+        public string id;
+        public string path;
+    }
+
+    // Used by the database to understand the data and persist it
+    [System.Serializable]
+    public class MetaData
+    {
+        public string key;
+        public object value;
+        public bool isReadonly;
+        public bool persist;
+    }
+}
---
+++ Assets/Game/Scripts/Framework/Common/Util.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+using Framework.Log;
+
+namespace Framework.Utils
+{
+    public static class Util 
+    {
+        private static volatile object writeLock = new object();
+
+        public static T ReadBinary<T>(string path)
+        {
+            var binaries = File.ReadAllBytes(path);
+            return Util.ByteArrayTo<T>(binaries);
+        }
+
+        public static void WriteBinary(object obj, string path, bool seperateThread = true)
+        {
+            if (seperateThread)
+            {
+                var t = new Thread(() => WriteObjectAtPath(obj, path));
+                t.Start();
+            }
+            else
+            {
+                WriteObjectAtPath(obj, path);
+            }
+        }
+
+        public static byte[] ObjectToByteArray(object obj)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        public static T ByteArrayTo<T>(byte[] arrBytes)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var binForm = new BinaryFormatter();
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                var obj = binForm.Deserialize(memStream);
+                return (T)obj;
+            }
+        }
+
+        public static T ReadFromResources<T>(string path) where T : UnityEngine.Object
+        {
+            return Resources.Load<T>(path);
+        }
+
+        public static UnityEngine.Object ReadFromResources(string path, Type type)
+        {
+            return Resources.Load(path, type);
+        }
+
+        public static T ReadJsonFromResources<T>(string path)
+        {
+            var json = Resources.Load<TextAsset>(path);
+            return JsonConvert.DeserializeObject<T>(json.text);
+        }
+
+        public static object ReadJsonFromResources(string path, Type type)
+        {
+            var json = Resources.Load<TextAsset>(path);
+            return JsonConvert.DeserializeObject(json.text, type);
+        }
+
+        private static void WriteObjectAtPath(object obj, string path)
+        {
+            lock (writeLock)
+            {
+                var b = Util.ObjectToByteArray(obj);
+                File.WriteAllBytes(path, b);
+                LogWrapper.Log(string.Format("binaries successfully saved to: {1}", path));
+            }
+        }
+    }
+}
