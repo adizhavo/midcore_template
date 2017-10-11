6a9ed523ac0c431ccf7c9fc1ef631fe8dd161183
Adi Zhavo <adi@gram.gs>
2017-10-11 19:02:34 +0100

    Add database name and app config path in constants

--- Assets/Game/Scripts/Framework/Common/Shared.cs
+++ Assets/Game/Scripts/Framework/Common/Shared.cs
@@ -2,7 +2,12 @@
 
 public partial class Constants
 {
+    // ids
     public const string APP_CONFIG_ID = "app_config";
+    public const string DATABASE_ID = "game_database";
+
+    // paths
+    public const string APP_CONFIG_PATH = "app_config";
 }
 
 namespace Framework
