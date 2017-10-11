c32a13a75ba1f6e4feaaba530f6fe9ac5c49dbdf
Adi Zhavo <adi@gram.gs>
2017-10-11 12:13:22 +0100

    Add database 0.0.1 and utility functions for read/write binaries

---
+++ Assets/Game/Scripts/Framework/Data/DataBase.cs
@@ -0,0 +1,64 @@
+﻿using Entitas;
+using UnityEngine;
+using Framework.Utils;
+using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace Framework.Data
+{
+    public class DataBase : IInitializeSystem
+    {
+        private Dictionary<string, object> data;
+        private string databasePath;
+
+        public DataBase()
+        {
+            data = new Dictionary<string, object>();
+        }
+
+        #region IInitializeSystem implementation
+
+        public void Initialize()
+        {
+            // TODO : get this value from the application config
+            databasePath = Path.Combine(Application.persistentDataPath, "data_base");
+
+            if (File.Exists(databasePath))
+            {
+                data = Util.ReadBinary<Dictionary<string, object>>(databasePath);
+            }
+            else
+            {
+                Flush();
+            }
+        }
+
+        #endregion
+
+        public void Set<T>(string key, object value, bool flush = true)
+        {
+            if (data.ContainsKey(key))
+            {
+                data[key] = value;
+            }
+            else
+            {
+                data.Add(key, value);
+            }
+
+            if (flush) Flush();
+        }
+
+        public T Get<T>(string key, T defaultValue = default(T))
+        {
+            return data.ContainsKey(key) ? (T)data[key] : defaultValue;
+        }
+
+        public void Flush()
+        {
+            Util.WriteBinary(data, databasePath);
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Framework/Utilities.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+
+namespace Framework.Utils
+{
+    public static class Util 
+    {
+        private static volatile object writeLock = new object();
+
+        public static T ReadBinary<T>(string path)
+        {
+            var binaries = File.ReadAllBytes(path);
+            return Util.ByteArrayTo<T>(binaries);
+        }
+
+        public static void WriteBinary(object obj, string path, bool seperateThread = true)
+        {
+            if (seperateThread)
+            {
+                var t = new Thread(() => WriteObjectAtPath(obj, path));
+                t.Start();
+            }
+            else
+            {
+                WriteObjectAtPath(obj, path);
+            }
+        }
+
+        public static byte[] ObjectToByteArray(object obj)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        public static T ByteArrayTo<T>(byte[] arrBytes)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var binForm = new BinaryFormatter();
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                var obj = binForm.Deserialize(memStream);
+                return (T)obj;
+            }
+        }
+
+        private static void WriteObjectAtPath(object obj, string path)
+        {
+            lock (writeLock)
+            {
+                var b = Util.ObjectToByteArray(obj);
+                File.WriteAllBytes(path, b);
+            }
+        }
+    }
+}
