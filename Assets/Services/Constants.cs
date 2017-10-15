aafe8e01501d8c4679cfc937046e31eddf9e7f10
Adi Zhavo <adi@gram.gs>
2017-10-15 20:25:54 +0100

    Add database id in the app config json file

--- Assets/Services/Constants.cs
+++ Assets/Services/Constants.cs
@@ -2,7 +2,6 @@
 {
     // ids
     public const string APP_CONFIG_ID = "app_config";
-    public const string DATABASE_ID = "game_database";
 
     // paths
     public const string APP_CONFIG_PATH = "Data/app_config";
