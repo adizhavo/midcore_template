0e967d75511e3217342a6068558ab9a2e4c9bd9e
Adi Zhavo <adi@gram.gs>
2017-10-15 20:21:54 +0100

    Update core constants

--- Assets/Services/Constants.cs
+++ Assets/Services/Constants.cs
@@ -5,6 +5,6 @@
     public const string DATABASE_ID = "game_database";
 
     // paths
-    public const string APP_CONFIG_PATH = "app_config";
+    public const string APP_CONFIG_PATH = "Data/app_config";
     public const string VERSION_LOG_PATH = "version_log";
 }
