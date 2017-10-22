13fdc51d0c3416c81cc33f37c5f4273890819acb
Adi Zhavo <adi@gram.gs>
2017-10-22 20:24:52 +0100

    Made constants file more readable

--- Assets/Services/Shared/Constants.cs
+++ Assets/Services/Shared/Constants.cs
@@ -2,14 +2,18 @@
 {
     // ids
     public const string APP_CONFIG_ID = "app_config";
+
     public const string TILED_MAP_DIR_ID = "TILED_map_dir";
+
+    public const string TILED_TILE_LAYER = "Tiles";
+
+    public const string TILED_OBJECTS_LAYER = "Objects";
 
     // paths
     public const string APP_CONFIG_PATH = "Data/app_config";
+
     public const string VERSION_LOG_PATH = "version_log";
 
     // Misc
     public const string JSON_FILE_EXTENSION = ".json";
-    public const string TILED_TILE_LAYER = "Tiles";
-    public const string TILED_OBJECTS_LAYER = "Objects";
 }
