7e1e35d6c8aee70d524d2c3b54322860729d2a03
Adi Zhavo <adi@gram.gs>
2017-10-22 20:17:47 +0100

    Change class name from TILED_Adapter to TILED_DataProvider

--- Assets/Services/Game/TILED/TILED_DataProvider.cs
+++ Assets/Services/Game/TILED/TILED_DataProvider.cs
@@ -12,7 +12,7 @@ namespace Services.Game.Tiled
     /// The assumption is that the folder containing the tilesets will always be in the same folder of the levels
     /// </summary>
 
-    public class TILED_Adapter
+    public class TILED_DataProvider
     {
         [Inject] DatabaseService database;
 
@@ -130,6 +130,11 @@ namespace Services.Game.Tiled
         public string GetMapTilesetObjectId(string mapPath, int gid)
         {
             return GetMapTilesetProperties(mapPath, gid).objectId;
+        }
+
+        public string GetMapTilesetTileTypeId(string mapPath, int gid)
+        {
+            return GetMapTilesetProperties(mapPath, gid).tileId;
         }
 
         // -------------------------------------------
