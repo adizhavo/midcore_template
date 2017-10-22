decaee20f98255e4ac4fa9089131c9e367177289
Adi Zhavo <adi@gram.gs>
2017-10-22 20:30:41 +0100

    Improve exception message for not found TILED layer

--- Assets/Services/Game/TILED/TILED_MapReader.cs
+++ Assets/Services/Game/TILED/TILED_MapReader.cs
@@ -18,7 +18,7 @@ namespace Services.Game.Tiled
             var tileLayer = tiledDataProvider.GetMapLayer(mapPath, Constants.TILED_TILE_LAYER);
 
             if (tileLayer == null)
-                throw new NullReferenceException("Could not file layer in the map " + mapPath + " with name " + Constants.TILED_TILE_LAYER);
+                throw new NullReferenceException("Could not file layer in the map " + mapPath + " with name " + Constants.TILED_TILE_LAYER + ", please add a layer with that name.");
 
             for (int i = 0; i < tileLayer.data.Length; i++)
             {
@@ -45,7 +45,7 @@ namespace Services.Game.Tiled
             var objectLayer = tiledDataProvider.GetMapLayer(mapPath, Constants.TILED_OBJECTS_LAYER);
 
             if (objectLayer == null)
-                throw new NullReferenceException("Could not file layer in the map " + mapPath + " with name " + Constants.TILED_OBJECTS_LAYER);
+                throw new NullReferenceException("Could not file layer in the map " + mapPath + " with name " + Constants.TILED_OBJECTS_LAYER + ", please add a layer with that name.");
 
             for (int i = 0; i < objectLayer.data.Length; i++)
             {
