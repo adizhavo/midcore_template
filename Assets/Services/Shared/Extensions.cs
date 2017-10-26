2359b3d61a3fb009b2095860e83eb3b01e411dc7
Adi Zhavo <adi@gram.gs>
2017-10-26 21:17:13 +0100

    Made extension methods for converting vector functions from custom to unity type

--- Assets/Services/Shared/Extensions.cs
+++ Assets/Services/Shared/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace Services.Core
 {
@@ -26,6 +27,46 @@ namespace Services.Core
             }
 
             return collected;
+        }
+
+        public static FloatVector2 ToFloatVector2(this Vector2 vector2)
+        {
+            return new FloatVector2(vector2.x, vector2.y);
+        }
+
+        public static Vector2 ToVector2(this FloatVector2 vector2)
+        {
+            return new Vector2(vector2.x, vector2.y);
+        }
+
+        public static FloatVector3 ToFloatVector3(this Vector3 vector3)
+        {
+            return new FloatVector3(vector3.x, vector3.y, vector3.z);
+        }
+
+        public static Vector3 ToVector3(this FloatVector3 vector3)
+        {
+            return new Vector3(vector3.x, vector3.y, vector3.z);
+        }
+
+        public static IntVector2 ToIntVector2(this Vector2 vector2)
+        {
+            return new IntVector2((int)vector2.x, (int)vector2.y);
+        }
+
+        public static Vector2 ToVector2(this IntVector2 vector2)
+        {
+            return new Vector2(vector2.x, vector2.y);
+        }
+
+        public static IntVector3 ToIntVector3(this Vector3 vector3)
+        {
+            return new IntVector3((int)vector3.x, (int)vector3.y, (int)vector3.z);
+        }
+
+        public static Vector3 ToVector3(this IntVector3 vector3)
+        {
+            return new Vector3(vector3.x, vector3.y, vector3.z);
         }
     }
 }
