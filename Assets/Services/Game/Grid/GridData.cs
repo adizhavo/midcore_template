e7b77142e9eca7155cebbede14fd457bee4224d4
Adi Zhavo <adi@gram.gs>
2017-10-26 21:18:29 +0100

    Move cell class in the GridData source file

--- Assets/Services/Game/Grid/GridData.cs
+++ Assets/Services/Game/Grid/GridData.cs
@@ -1,35 +1,10 @@
-﻿using Services.Core;
+﻿using Entitas;
+using Services.Core;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Services.Game.Grid
 {
-    public class GridSettings
-    {
-        public enum GridPivot
-        {
-            TOP_LEFT,
-            TOP_RIGHT,
-            BOTTOM_LEFT,
-            BOTTOM_RIGHT,
-            CENTER
-        }
-
-        public FloatVector3 startPos = new FloatVector3(0f, 0f, 0f);
-        public FloatVector2 cellSpacing = new FloatVector2(1f, 1f);
-        public GridPivot pivot = GridPivot.TOP_LEFT;
-
-        public GridSettings()
-        {
-        }
-
-        public GridSettings(FloatVector3 startPos, FloatVector2 cellSpacing, GridPivot pivot)
-        {
-            this.startPos = startPos;
-            this.cellSpacing = cellSpacing;
-            this.pivot = pivot;
-        }
-    }
-
     public class GridData
     {
         public string mapPath;
@@ -42,6 +17,46 @@ namespace Services.Game.Grid
             this.mapPath = mapPath;
             this.size = size;
             this.settings = settings;
+        }
+    }
+
+    public class Cell
+    {
+        public int row;
+        public int column;
+        public string typeId;
+        public string objectId;
+        public GameEntity occupant;
+        public GameEntity entity;
+
+        public Cell(string typeId)
+        {
+            this.typeId = typeId;
+        }
+    }
+
+    public class NullCell : Cell
+    {
+        public NullCell() : base(string.Empty)
+        {
+            row = -1;
+            column = -1;
+        }
+    }
+
+    public class GridSettings
+    {
+        public FloatVector3 startPos = new FloatVector3(0f, 0f, 0f);
+        public FloatVector2 cellSpacing = new FloatVector2(1f, 1f);
+
+        public GridSettings()
+        {
+        }
+
+        public GridSettings(FloatVector3 startPos, FloatVector2 cellSpacing)
+        {
+            this.startPos = startPos;
+            this.cellSpacing = cellSpacing;
         }
     }
 }
