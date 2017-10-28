2766dab4f2ba520a1382291fdbfcc1f95e301084
Adi Zhavo <adi@gram.gs>
2017-10-28 17:03:55 +0100

    Show the cell unique id when creating the cell

--- Assets/Services/Game/Grid/GridService.cs
+++ Assets/Services/Game/Grid/GridService.cs
@@ -325,7 +325,7 @@ namespace Services.Game.Grid
                 cell.xPosition += cell.cell.row * grid.settings.cellSpacing.x;
                 cell.zPosition -= cell.cell.column * grid.settings.cellSpacing.y;
                 #if UNITY_EDITOR
-                cell.viewObject.name = string.Format("cell_{0}_{1}_{2}_{3}", cell.objectId, cell.typeId, cell.row, cell.column);
+                cell.viewObject.name = string.Format("cell_{0}_{1}_{2}_{3}_{4}", cell.objectId, cell.typeId, cell.row, cell.column, cell.uniqueId);
                 #endif
             }
         }
