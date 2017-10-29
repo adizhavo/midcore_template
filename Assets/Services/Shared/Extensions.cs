5e0f99ec0cd4330f8a0036e528ff374c26cc1563
Adi Zhavo <adi@gram.gs>
2017-10-29 13:58:04 +0000

    Add extension methods for UI elements

--- Assets/Services/Shared/Extensions.cs
+++ Assets/Services/Shared/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 namespace Services.Core
@@ -67,6 +68,98 @@ namespace Services.Core
         public static Vector3 ToVector3(this IntVector3 vector3)
         {
             return new Vector3(vector3.x, vector3.y, vector3.z);
+        }
+
+        public static void SetDefaultScale(this RectTransform trans)
+        {
+            trans.localScale = Vector3.one;
+        }
+
+        public static void SetPivotAndAnchors(this RectTransform trans, Vector2 aVec)
+        {
+            trans.pivot = aVec;
+            trans.anchorMin = aVec;
+            trans.anchorMax = aVec;
+        }
+
+        public static Vector2 GetSize(this RectTransform trans)
+        {
+            return trans.rect.size;
+        }
+
+        public static float GetWidth(this RectTransform trans)
+        {
+            return trans.rect.width;
+        }
+
+        public static float GetHeight(this RectTransform trans)
+        {
+            return trans.rect.height;
+        }
+
+        public static void SetAlpha(this RectTransform trans, float value) {
+            var image = trans.GetComponent<Image>();
+            Color c = image.color;
+            c.a = value;
+            image.color = c;
+        }
+
+        public static void SetPositionOfPivot(this RectTransform trans, Vector2 newPos)
+        {
+            trans.localPosition = new Vector3(newPos.x, newPos.y, trans.localPosition.z);
+        }
+
+        public static void SetX(this RectTransform trans, float xPos)
+        {
+            trans.position = new Vector3(xPos, trans.position.y, trans.position.z);
+        }
+
+        public static void SetY(this RectTransform trans, float yPos)
+        {
+            trans.position = new Vector3(trans.position.x, yPos, trans.position.z);
+        }
+
+        public static void SetLeftBottomPosition(this RectTransform trans, Vector2 newPos)
+        {
+            trans.localPosition = new Vector3(newPos.x + (trans.pivot.x*trans.rect.width),
+                newPos.y + (trans.pivot.y*trans.rect.height), trans.localPosition.z);
+        }
+
+        public static void SetLeftTopPosition(this RectTransform trans, Vector2 newPos)
+        {
+            trans.localPosition = new Vector3(newPos.x + (trans.pivot.x*trans.rect.width),
+                newPos.y - ((1f - trans.pivot.y)*trans.rect.height), trans.localPosition.z);
+        }
+
+        public static void SetRightBottomPosition(this RectTransform trans, Vector2 newPos)
+        {
+            trans.localPosition = new Vector3(newPos.x - ((1f - trans.pivot.x)*trans.rect.width),
+                newPos.y + (trans.pivot.y*trans.rect.height), trans.localPosition.z);
+        }
+
+        public static void SetRightTopPosition(this RectTransform trans, Vector2 newPos)
+        {
+            trans.localPosition = new Vector3(newPos.x - ((1f - trans.pivot.x)*trans.rect.width),
+                newPos.y - ((1f - trans.pivot.y)*trans.rect.height), trans.localPosition.z);
+        }
+
+        public static void SetSize(this RectTransform trans, Vector2 newSize)
+        {
+            Vector2 oldSize = trans.rect.size;
+            Vector2 deltaSize = newSize - oldSize;
+            trans.offsetMin = trans.offsetMin - new Vector2(deltaSize.x*trans.pivot.x, deltaSize.y*trans.pivot.y);
+            trans.offsetMax = trans.offsetMax +
+                new Vector2(deltaSize.x*(1f - trans.pivot.x), deltaSize.y*(1f - trans.pivot.y));
+        }
+
+        public static void SetWidth(this RectTransform trans, float newSize)
+        {
+            SetSize(trans, new Vector2(newSize, trans.rect.size.y));
+        }
+
+        public static void SetHeight(this RectTransform trans, float newSize)
+        {
+            SetSize(trans, new Vector2(trans.rect.size.x, newSize));
         }
     }
 }
