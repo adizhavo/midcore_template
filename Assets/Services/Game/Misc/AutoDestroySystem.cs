ab66015561d265071b5a24f27602570b3803ac21
Adi Zhavo <adi@gram.gs>
2017-11-04 22:17:58 +0000

    Add auto destroy component and system

---
+++ Assets/Services/Game/Misc/AutoDestroySystem.cs
@@ -0,0 +1,26 @@
+﻿using Entitas;
+using UnityEngine;
+
+namespace Services.Game.Misc
+{
+    public class AutoDestroySystem : IExecuteSystem
+    {
+        #region IExecuteSystem implementation
+
+        public void Execute()
+        {
+            var entites = Contexts.sharedInstance.game.GetEntities(GameMatcher.AutoDestroy);
+            foreach(var entity in entites)
+            {
+                entity.autoDestroy.remainingTime -= entity.autoDestroy.ignoreTimescale ? Time.unscaledDeltaTime : Time.deltaTime;
+                if (entity.autoDestroy.remainingTime < 0f)
+                {
+                    entity.Destroy();
+                }
+            }
+        }
+
+        #endregion
+        
+    }
+}
