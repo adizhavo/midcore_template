b7c6994df7c64f22f12fc18e73126e81fc57c710
Adi Zhavo <adi@gram.gs>
2017-11-04 22:18:37 +0000

    Add VFXData structure

---
+++ Assets/Services/Game/Data/VFXData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Services.Game.Data
+{
+    public class VFXData
+    {
+        public string id;
+        public string prefab;
+        public float disableTime;
+        public bool ignoreTimescale;
+    }
+}
+
