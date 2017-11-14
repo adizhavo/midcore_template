b66d72712e2280db4d6a19730d034b931c5b4964
Adi Zhavo <adi@gram.gs>
2017-11-14 17:54:59 +0000

    Add editor tools only for clearing data atm

---
+++ Assets/Game/Editor/Tools.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Services.Core;
+using Services.Core.Data;
+using System.IO;
+
+namespace GramGames.MergeKingdom.Tools
+{
+    public class Tools
+    {
+        [MenuItem("Tools/Clear PlayerPrefs")]
+        private static void ClearPlayerPrefs()
+        {
+            PlayerPrefs.DeleteAll();
+            var appConfig = Utils.ReadJsonFromResources<ApplicationConfig>(Constants.PATH_APP_CONFIG);
+            var databasePath = Path.Combine(Application.persistentDataPath, appConfig.databaseId);
+            if (File.Exists(databasePath))
+            {
+                File.Delete(databasePath);
+            }
+            Debug.Log("All data cleared!");
+        }
+    }
+}
