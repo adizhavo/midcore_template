bc5d1654c6b77774cc17eb7bef89468db495d743
Adi Zhavo <adi@gram.gs>
2017-11-14 16:52:59 +0000

    Add tutorial scene bindings

---
+++ Assets/Services/Game/Tutorial/SceneBindings/DialogBindingComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Services.Core.Databinding;
+using Services.Core.Databinding.Components;
+
+namespace Services.Game.Tutorial.Bindings
+{
+    public class DialogBindingComponent : MonoBindingComponent<MonoBehaviour, TutorialDialogType>
+    {
+        [System.Serializable]
+        public class SceneDialogs
+        {
+            public TutorialDialogType type;
+            public GameObject dialog;
+        }
+
+        public SceneDialogs[] dialogs;
+
+        #region BindingComponent implementation
+
+        public override void OnValueChanged(string branch, TutorialDialogType value)
+        {
+            foreach(var dialog in dialogs)
+            {
+                dialog.dialog.SetActive(value.Equals(dialog.type));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Services/Game/Tutorial/SceneBindings/TutorialButtonBindingComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using Services.Core.Databinding.Components;
+
+namespace Services.Game.Tutorial.Bindings
+{
+    public class TutorialButtonBindingComponent : ButtonInteractionBindingComponent
+    {
+        protected override void Start()
+        {
+            base.Start();
+            component.onClick.AddListener(() => TutorialService<TutorialStep>.Notify("move_next"));
+        }
+    }
+}
