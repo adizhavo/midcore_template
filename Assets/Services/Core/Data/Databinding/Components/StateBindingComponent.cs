bb3a93ec26238544637e98b26ca4fab9dc25fd46
Adi Zhavo <adi@gram.gs>
2018-06-12 10:56:14 +0100

    Add state binding component

---
+++ Assets/Services/Core/Data/Databinding/Components/StateBindingComponent.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Services.Core.Databinding.Components
+{
+	public class StateBindingComponent : MonoBindingComponent<MonoBehaviour, string>
+	{
+		[System.Serializable]
+		public class States
+		{
+			public string id;
+			public GameObject[] enable;
+		}
+
+		public States[] states;
+		
+		public override void OnValueChanged(string branch, string value)
+		{
+			foreach (var state in states)
+			{
+				foreach (var o in state.enable)
+				{
+					o.SetActive(false);
+				}
+			}
+
+			foreach (var state in states)
+			{
+				if (string.Equals(state.id, value))
+				{
+					foreach (var o in state.enable)
+					{
+						o.SetActive(true);
+					}
+					break;
+				}
+			}
+		}
+	}
+}
