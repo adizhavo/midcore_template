350f6dcf6f025a6a81f20f1ddced1e85dc298567
Adi Zhavo <adi@gram.gs>
2018-12-23 13:27:29 +0000

    Add utility scripts

---
+++ Assets/Game/Scripts/Shared/ObjectRotator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace MidcoreTemplate.Game.Utilities
+{
+	public class ObjectRotator : MonoBehaviour
+	{
+		public Transform RotateTr;
+		public Vector3 rotationSpeed;
+
+		private void LateUpdate()
+		{
+			RotateTr.localEulerAngles += rotationSpeed * Time.deltaTime;
+		}
+	}
+}
---
+++ Assets/Game/Scripts/Shared/RectSizeFollower.cs
@@ -0,0 +1,22 @@
+﻿using Services.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MidcoreTemplate.Game.Utilities
+{
+    [ExecuteInEditMode]
+    public class RectSizeFollower : MonoBehaviour
+    {
+        public RectTransform selectedSize;
+        public RectTransform followSize;
+
+        private void Update()
+        {
+            if (followSize != null && selectedSize != null)
+            {
+                selectedSize.SetWidth(followSize.GetWidth());
+                selectedSize.SetHeight(followSize.GetHeight());
+            }
+        }
+    }
+}
